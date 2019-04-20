b88e7658282e9e84d5a5ff24d0ee938a86793868
Adam Bene <adambhun@gmail.com>
2019-04-20 15:15:58 +0200

    ec2 repair

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const port = 9005;
+const port = 80;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => {
